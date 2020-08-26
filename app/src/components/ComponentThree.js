1999058f7419b76ccbae167fa6039aceac082539
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-25 19:21:52 -0700

    standard lorem component

---
+++ app/src/components/ComponentThree.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const style = {
+  background: "pink",
+  color: "white",
+  padding: "1.5rem",
+};
+
+const ComponentThree = () => {
+  return (
+    <>
+      <div style={style}>
+        <h1>Component3</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+          velit ex nisi cumque itaque maxime alias nobis eos commodi sed
+          reiciendis, harum non corporis sapiente delectus veritatis. Corrupti,
+          inventore eius! Sapiente temporibus blanditiis placeat quae doloremque
+          debitis sed? Voluptate, commodi ipsam earum nihil explicabo quis
+          perferendis molestias repellendus autem fugit necessitatibus
+          aspernatur voluptas modi, saepe blanditiis, incidunt fugiat? Earum,
+          obcaecati. Nam unde porro omnis, alias, non aliquid iure accusantium,
+          molestias sed earum sint libero quos consectetur dicta. Eveniet, odio,
+          omnis consectetur alias doloribus officiis officia beatae architecto
+          perspiciatis itaque a! Quod libero architecto dolores harum enim
+          deleniti illum voluptatibus velit nam nostrum deserunt atque, unde
+          vitae in cum non doloribus earum dicta perferendis quos natus! Maiores
+          quis temporibus labore cum! Placeat inventore voluptates officia
+          deleniti eligendi, error porro autem laudantium repellendus delectus
+          qui minus libero esse cumque aliquid voluptatibus aperiam commodi
+          reprehenderit laboriosam culpa. Minus exercitationem inventore
+          pariatur assumenda officiis. Dolores id cumque magni rerum ad non
+          necessitatibus dolorum fugit inventore accusantium! Recusandae
+          cupiditate tempora architecto repudiandae quidem corporis temporibus
+          hic, deserunt quas alias vel harum, sed error magni nihil. Nihil
+          perspiciatis corporis ducimus voluptate incidunt, dolore asperiores?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ComponentThree;
